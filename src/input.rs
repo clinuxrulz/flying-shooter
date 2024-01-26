22458cbc1257ed29782abb87a4cb90487f78c34b
Clinton Selke <clinuxrulz@gmail.com>
2024-01-27 00:44:57 +1000

    wip

--- src/input.rs
+++ src/input.rs
@@ -1,4 +1,4 @@
-use crate::game::Config;
+use crate::{components::{FaceDir, Player}, game::Config};
 use bevy::{prelude::*, utils::HashMap};
 use bevy_ggrs::{LocalInputs, LocalPlayers};
 use virtual_joystick::*;
@@ -9,20 +9,50 @@ const INPUT_LEFT: u8 = 1 << 2;
 const INPUT_RIGHT: u8 = 1 << 3;
 const INPUT_FIRE: u8 = 1 << 4;
 
+const ROTATE_SPEED: f32 = 0.1;
+
 pub fn read_local_inputs(
     mut commands: Commands,
     keys: Res<Input<KeyCode>>,
     local_players: Res<LocalPlayers>,
+    players: Query<(&Player, &FaceDir)>,
     mut joystick: EventReader<VirtualJoystickEvent<String>>,
+    time: Res<Time>
 ) {
     let mut local_inputs = HashMap::new();
 
-    for j in joystick.read() {
-    }
-
+    let angle_diff = |a: f32, b: f32| -> f32 {
+        //     double diff = ( angle2 - angle1 + 180 ) % 360 - 180;
+        // return diff < -180 ? diff + 360 : diff;
+        let diff = (b - a + std::f32::consts::PI) % (2.0 * std::f32::consts::PI) - std::f32::consts::PI;
+        if diff < -std::f32::consts::PI {
+            diff + 2.0 * std::f32::consts::PI
+        } else {
+            diff
+        }
+    };
+    let min_diff = time.delta_seconds() * ROTATE_SPEED * 10.0;
     for handle in &local_players.0 {
         let mut input = 0u8;
-
+        for j in joystick.read().take(1) {
+            let axis = j.axis();
+            if axis.x == 0.0f32 && axis.y == 0.0f32 {
+                continue;
+            }
+            let target_face_dir = (-axis.y).atan2(-axis.x);
+            for (player, face_dir) in &players {
+                if player.handle != *handle {
+                    continue;
+                }
+                let face_dir = face_dir.0;
+                let diff = angle_diff(face_dir, target_face_dir);
+                if diff < -min_diff {
+                    input |= INPUT_LEFT;
+                } else if diff > min_diff {
+                    input |= INPUT_RIGHT;
+                }
+            }
+        }
         if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
             input |= INPUT_UP;
         }
@@ -38,14 +68,11 @@ pub fn read_local_inputs(
         if keys.any_pressed([KeyCode::Space, KeyCode::Return]) {
             input |= INPUT_FIRE;
         }
-
         local_inputs.insert(*handle, input);
     }
 
     commands.insert_resource(LocalInputs::<Config>(local_inputs));
 }
-
-const ROTATE_SPEED: f32 = 0.1;
 
 pub fn rotate_by(input: u8) -> f32 {
     if (input & INPUT_LEFT) != 0 {
